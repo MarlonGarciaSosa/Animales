214b2d92f3fb1f1721c66a2764fcb117027c87d9
Marlon <155335960+MarlonGarciaSosa@users.noreply.github.com>
2024-01-11 17:11:20 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,15 +1,54 @@
 function CambiarColorSol(){
-    const XD = document.querySelector(".CambiarColor")  
-    XD.style.color = "white"
-    const sol = document.querySelector(".sol")
-    sol.setAttribute("src", "luna.png")
-    sol.setAttribute("onclick", "CambiarColorLuna()")
-    
-}
+    const XD = document.querySelector(".CambiarColor");
+    XD.style.color = "white";
+    const sol = document.querySelector(".sol");
+    sol.setAttribute("src", "luna.png");
+    sol.setAttribute("onclick", "CambiarColorLuna()");
+};
+
 function CambiarColorLuna(){
-     const XD = document.querySelector(".CambiarColor")
-     XD.style.color = "black"
-     const sol = document.querySelector(".sol")
-     sol.setAttribute("src", "sol.png")
-     sol.setAttribute("onclick", "CambiarColorSol()")
- }
+    const XD = document.querySelector(".CambiarColor");
+    XD.style.color = "black";
+    const sol = document.querySelector(".sol");
+    sol.setAttribute("src", "sol.png");
+    sol.setAttribute("onclick", "CambiarColorSol()");
+};
+
+function AbrirPestaña(){
+    const ElementoTexto = document.createElement("TEXTO");
+    const padre = document.querySelector(".padre");
+    const Carpeta = document.querySelector(".Almacen") ;
+    ElementoTexto.innerHTML =`<table id="tabla3">
+    <tr>
+        <th><img src="Aves.jpg"></th>
+        <th><img src="Reptil.jpg"></th>
+        <th><img src="Gatos.jpg"></th>                                                                                        
+    </tr>
+    <tr>
+        <td><p>Aves</p></td>
+        <td><p>Reptiles</p></td>
+        <td><p>Gatos</p></td>
+    </tr>
+</table>
+<table id="tabla4">
+     <tr>
+        <th><img src="Perros.jpg"></th>
+        <th><img src="Conejos.jpg"></th>
+     </tr>
+     <tr>
+        <td><p>Perros</p></td>
+        <td><p>Conejos</p></td>
+     </tr>
+</table>`;
+    ElementoTexto.classList.add("Texto");
+    padre.appendChild(ElementoTexto);
+    Carpeta.setAttribute("onclick", "Cerrar()");
+};
+
+function Cerrar(){
+    const padre = document.querySelector(".padre");
+    const Carpeta = document.querySelector(".Almacen");
+    const Chorizo = document.querySelector(".Texto");
+    Carpeta.setAttribute("onclick", "AbrirPestaña()");    
+    padre.removeChild(Chorizo);
+};
